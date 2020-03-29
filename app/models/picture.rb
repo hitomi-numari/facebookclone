871f08f4c4ba789deb10bcf7f2bf80d446f753e0
hitomi numari <h.h.hitomi1216@gmail.com>
2020-03-30 01:49:06 +0900

    Release to heroku

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,7 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   validates :image_or_content, presence: true
+  attr_accessor :action_required
 
   private
     def image_or_content
