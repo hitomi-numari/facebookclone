7dca5c9558f1579c4485b2682f809aeea729cd65
hitomi numari <h.h.hitomi1216@gmail.com>
2020-03-30 14:15:55 +0900

    herokuデプロイ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_134740) do
+ActiveRecord::Schema.define(version: 2020_03_30_010054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_134740) do
     t.datetime "updated_at", null: false
     t.date "birthday"
     t.integer "gender"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
